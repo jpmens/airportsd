8136a1bac9efcec037b1b870b74a49cfbd33511b
Jan-Piet Mens <jp@mens.de>
2020-10-01 21:49:13 +0200

    convert dat to cdb input

---
+++ dat2cdb.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Jan-Piet Mens, October 2020
+
+# read airports.dat CSV and produce key_JSON for passing to cdb(1)
+
+import csv
+import json
+
+with open('airports.dat', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        (id, name, city, country, iata, icao, lat, lon, alt, tz, dst, tzdb, typ, source) = row
+        if iata != "\\N":
+            data = {
+                "iata"   : iata,
+                "cc"     : country,
+                "city"   : city,
+                "lat"    : lat,
+                "lon"    : lon,
+            }
+            print("{iata} {data}".format(iata=iata.upper(), data=json.dumps(data)))
+        
