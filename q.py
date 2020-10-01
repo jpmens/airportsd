9688b2dbfdd3024dd54a2032489146304e7b5450
Jan-Piet Mens <jp@mens.de>
2020-10-01 21:49:53 +0200

    initial version

---
+++ q.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# simple query utility for airportd
+
+import sys
+import requests
+import json
+
+url = "http://127.0.0.1:8812/lookup?iata={iata}"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {p} IATA".format(p=sys.argv[0]))
+        exit(2)
+
+    r = requests.get(url.format(iata=sys.argv[1]))
+    if r.status_code == 200:
+        print(json.dumps(r.json(), indent=4))
+    else:
+        print(r.status_code)
